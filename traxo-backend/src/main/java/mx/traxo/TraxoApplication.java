2a9e42ff706a721c6529293d3181116ee8196723
Jesús A. Toledo <jeatog@hotmail.com>
2026-03-05 23:15:46 -0600

    [Backend] Ajustes de Configuración
    - Se quita validación de UserDetailsService.
    - Se evitan queries lazy inesperadas con open-in-view para peticiones HTTP.
    - Se elimina la linea dialect de Hibernate.

--- traxo-backend/src/main/java/mx/traxo/TraxoApplication.java
+++ traxo-backend/src/main/java/mx/traxo/TraxoApplication.java
@@ -2,9 +2,10 @@ package mx.traxo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 @EnableCaching
 public class TraxoApplication {
     public static void main(String[] args) {
